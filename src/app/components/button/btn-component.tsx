443cb4cbdbb2bb0476cda290ae1ef1dd209c174e
Guga <gugakobakhidze18@gmail.com>
2024-03-20 06:36:43 +0400

    adding quiz and logics

--- src/app/components/button/btn-component.tsx
+++ src/app/components/button/btn-component.tsx
@@ -1,21 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { ButtonStyle } from "@/app/mainpage/HomePageStyle";
 import { IBtn } from "@/app/interfaces/interfaces";
 import { useRouter } from "next/navigation";
 import { Box } from "@mui/material";
 
-const BtnComponent: React.FC<IBtn> = ({ text, href, onClick }) => {
+const BtnComponent: React.FC<IBtn> = ({ text, href, onClick, checked }) => {
+  const [error, setError] = useState<boolean>(false);
   const router = useRouter();
 
   const startQuiz = () => {
     href && router.push(href);
   };
-  
+
+  const onBtnClick = () => {
+    !checked && setError(true);
+    onClick && onClick();
+  };
+
   return (
     <Box onClick={startQuiz}>
-      <ButtonStyle variant="contained">{text}</ButtonStyle>
+      <ButtonStyle
+        onClick={onBtnClick}
+        variant="contained"
+        sx={{
+          bgcolor: error ? "red" : "",
+          "&:hover": {
+            bgcolor: error ? "red" : "",
+          },
+        }}
+      >
+        {error ? "მონიშნეთ პასუხი" : text}
+      </ButtonStyle>
     </Box>
   );
 };
